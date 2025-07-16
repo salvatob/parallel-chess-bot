4b791a34149e2e82ad9442effac9ad911ec876cf
Tobiáš Salva <tobias@avlas.cz>
2025-07-16 17:03:52 +0200

    Implement an extension method, that flattens a 2D array

---
+++ UnitTests/MatrixExtensions.cs
@@ -0,0 +1,18 @@
+namespace TestProject1;
+
+public static class MatrixExtensions {
+    
+    public static TItem[] Flatten<TItem>(this TItem[,] matrix) {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        var result = new TItem[rows*cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i * rows + j] = matrix[i, j];
+            }
+        }
+
+        return result;
+    } 
+}
