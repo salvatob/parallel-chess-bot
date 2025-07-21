d33b8dc41363c7aa2d0b21426fdcbf482b69538c
Tobiáš Salva <tobias@avlas.cz>
2025-07-21 22:52:11 +0200

    Implement the Move structure

--- ChessBotCore/Move.cs
+++ ChessBotCore/Move.cs
@@ -2,10 +2,9 @@ namespace ChessBotCore;
 
 public struct Move {
     public  State StateAfter { get; }
-    private byte _info = 0;
-    // bit 0 is for capture
-    // bit 1 is for promotion
-    // bit 2 is for check
+    public required bool IsCapture { get; init; }
+    public bool IsPromotion { get; init; }
+    public bool IsCheck { get; init; }
 
     
     public Move(State stateAfter) {
