9207004113ab786684b332a1368df3081a0d6725
Tobiáš Salva <tobias@avlas.cz>
2025-07-25 00:44:26 +0200

    Begin implementing a ready check for Move.cs

--- ChessBotCore/Move.cs
+++ ChessBotCore/Move.cs
@@ -1,7 +1,21 @@
 namespace ChessBotCore;
 
 public struct Move {
-    public  State StateAfter { get; set; }
+    [Flags]
+    private enum MoveInit : byte {
+        None     = 0,
+        FromSet  = 1 << 0,
+        ToSet    = 1 << 1,
+        FlagSet  = 1 << 2,
+        PromoSet = 1 << 3,
+        All      = FromSet | ToSet | FlagSet | PromoSet
+    }
+
+    public  State StateAfter {
+        get;
+            
+        set ;
+    }
     public required bool IsCapture { get; init; }
     public bool IsPromotion { get; init; }
     public bool IsCheck { get; init; }
