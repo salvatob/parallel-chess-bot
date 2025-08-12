e7567cb7e2f7aab546dc10f07a23b9c170284d2b
Tobiáš Salva <tobias@avlas.cz>
2025-08-12 21:33:43 +0200

    Add basic logic to filter out illegal moves

--- ChessBotCore/move_generators/GeneratorWrapper.cs
+++ ChessBotCore/move_generators/GeneratorWrapper.cs
@@ -7,6 +7,16 @@ public sealed class GeneratorWrapper : IMoveGenerator {
     public GeneratorWrapper(params IMoveGenerator[] generators) {
         _generators = generators;
     }
+
+    public static GeneratorWrapper Default => new GeneratorWrapper(
+        KingMoveGenerator.Instance,
+        KnightMoveGenerator.Instance,
+        RookMoveGenerator.Instance ,
+        // TODO
+        // PawnMoveGenerator.Instance,
+        QueenMoveGenerator.Instance,
+        BishopMoveGenerator.Instance
+    );
     
     public IEnumerable<Move> GenerateMoves(State state) {
         foreach (IMoveGenerator generator in _generators) {
@@ -14,5 +24,27 @@ public sealed class GeneratorWrapper : IMoveGenerator {
                 yield return move;
             }
         }
+    }
+
+    public IEnumerable<Move> GetLegalMoves(State state) {
+        return GenerateMoves(state).Where(CheckMoveLegality);
+
+    }
+
+    private bool CheckMoveLegality(Move move) {
+        // castles are already checked
+        if (move.IsCastle) return true;
+        
+        // if true, we are checking for white king
+        bool kingColor = !move.StateAfter.WhiteIsActive;
+        foreach (Move moveAfter in GenerateMoves(move.StateAfter)) {
+            if (kingColor) {
+                if (moveAfter.StateAfter.WhiteKing.IsEmpty()) return false;
+            } else {
+                if (moveAfter.StateAfter.BlackKing.IsEmpty()) return false;
+            }
+        }
+
+        return true;
     }
 }
