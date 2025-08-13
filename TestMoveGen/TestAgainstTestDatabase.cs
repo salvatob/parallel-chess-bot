7f920193223239be4425d7fa1688c7492cff7a5f
Tobiáš Salva <tobias@avlas.cz>
2025-08-13 13:05:20 +0200

    Rewrite move gen tests to run better in parallel

--- TestMoveGen/TestAgainstTestDatabase.cs
+++ TestMoveGen/TestAgainstTestDatabase.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Text.Json;
 using ChessBotCore;
 using FluentAssertions;
@@ -6,6 +7,29 @@ using TestMoveGen.testcases;
 namespace TestMoveGen;
 
 public class TestAgainstTestDatabase {
+    public static IEnumerable<object[]> TestCases => LoadTestCases();
+
+    
+    public static IEnumerable<object[]> LoadTestCases() {
+        string[] files = ["standard", "castling", "famous", "pawns", "promotions", "taxing"];
+        foreach (var fileName in files) {
+            
+            var path = $@"C:\Users\tobia\RiderProjects\ParallelChessBot\TestMoveGen\testcases\{fileName}.json";
+
+            var options = new JsonSerializerOptions {
+                PropertyNameCaseInsensitive = true
+            };
+            string json = new StreamReader(path).ReadToEnd();
+            RootObject? cases = JsonSerializer.Deserialize<RootObject>(json, options);
+
+            foreach (var c in cases.TestCases ) {
+                yield return [c];
+            }
+
+        }
+    }
+    
+    
     private string DeleteEnpassantFromFen(string fen) {
         var tokens = fen.Split(" ", 6);
         tokens[3] = "-";
@@ -13,31 +37,24 @@ public class TestAgainstTestDatabase {
     }
     
     [Theory]
-    [InlineData("standard")]
-    public void GetAllMoves(string fileName) {
+    // [InlineData("standard")]
+    [MemberData(nameof(TestCases))]
+    public void GetAllMoves(TestCases testCase) {
         //arrange
-        var path = $@"C:\Users\tobia\RiderProjects\ParallelChessBot\TestMoveGen\testcases\{fileName}.json";
+        var start = FenLoader.ParseFen(testCase.Start.Fen);
 
-        var cases = JsonSerializer.Deserialize<RootObject>(new StreamReader(path).ReadToEnd());
-
-
-        foreach (var testCase in cases.testCases) {
-            var start = FenLoader.ParseFen(testCase.start.fen);
-
-            IEnumerable<Move> moves = GeneratorWrapper.Default.GetLegalMoves(start);
-
-            var moveFens = moves.Select(m => FenCreator.GetFen(m.StateAfter));
-            IEnumerable<string> expected = 
-                from c in testCase.expected
-                where c.move != "0-0" && c.move != "0-0-0"
-                // select c.fen;
-                select DeleteEnpassantFromFen(c.fen);
-
-            moveFens.Should().BeEquivalentTo(expected);
-        }
+        IEnumerable<string> expected = 
+            from c in testCase.Expected
+            where c.Move != "0-0" && c.Move != "0-0-0"
+            // select c.fen;
+            select DeleteEnpassantFromFen(c.Fen);
         
         //act
-
+        IEnumerable<Move> moves = GeneratorWrapper.Default.GetLegalMoves(start);
+        
+        var moveFens = moves.Select(m => FenCreator.GetFen(m.StateAfter));
+        
         //assert
+        moveFens.Should().BeEquivalentTo(expected);
     }
 }
--- TestMoveGen/testcases/TestJsonObject.cs
+++ TestMoveGen/testcases/TestJsonObject.cs
@@ -1,20 +1,23 @@
 namespace TestMoveGen.testcases;
 public class RootObject {
-    public string description { get; set; }
-    public TestCases[] testCases { get; set; }
+    public string Description { get; set; }
+    public TestCases[] TestCases { get; set; }
 }
 
 public class TestCases {
-    public Start start { get; set; }
-    public Expected[] expected { get; set; }
+    public Start Start { get; set; }
+    public Expected[] Expected { get; set; } 
+    public override string ToString() => Start.Description;
+
 }
 
 public class Start {
-    public string description { get; set; }
-    public string fen { get; set; }
+    public string Description { get; set; }
+    public string Fen { get; set; }
+    // public override string ToString() => Description;
 }
 
 public class Expected {
-    public string move { get; set; }
-    public string fen { get; set; }
+    public string Move { get; set; }
+    public string Fen { get; set; }
 }
