5a1e0d6fdc9d79e4aabd6dc41341face3a9548a7
Tobiáš Salva <tobias@avlas.cz>
2025-09-01 22:28:32 +0200

    FIx comment

--- ChessBotCore/move_generators/IMoveGenerator.cs
+++ ChessBotCore/move_generators/IMoveGenerator.cs
@@ -2,7 +2,7 @@ namespace ChessBotCore;
 
 /// <summary>
 /// Provides an abstraction over move generation.
-/// In itself Should not be bound to the Singleton pattern. 
+/// In itself should not be bound to the Singleton pattern of some specific move generator implementations.
 /// </summary>
 public interface IMoveGenerator {
     public IEnumerable<Move> GenerateMoves(State state);
