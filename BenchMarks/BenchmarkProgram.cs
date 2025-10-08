b8698d715125fa4ed5f694aca9dfbe8dfd5b2edc
salvat <tobias@avlas.cz>
2025-10-09 00:16:17 +0200

    Rename benchmark main program

--- BenchMarks/BenchmarkProgram.cs
+++ BenchMarks/BenchmarkProgram.cs
@@ -2,16 +2,18 @@
 using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
 using BenchmarkDotNet.Toolchains.InProcess.Emit;
+using BenchmarkDotNet.Toolchains.InProcess.NoEmit;
 
 namespace Benchmarks;
 
 
-public class Program
+public class BenchmarkProgram
 {
     public static void Main(string[] args) {
         var inProcessConfig = ManualConfig.Create(DefaultConfig.Instance)
-            .AddJob(Job.Default.WithToolchain(InProcessEmitToolchain.Instance));
+            .AddJob(Job.Default.WithToolchain(InProcessNoEmitToolchain.Instance));
         // BenchmarkRunner.Run<RowMaskBench>(inProcessConfig);
-        BenchmarkRunner.Run<BitOperationMethods>(inProcessConfig);
+        // BenchmarkRunner.Run<BitOperationMethods>(inProcessConfig);
+        BenchmarkRunner.Run<AllocationExperiments>(inProcessConfig);
     }
 }
