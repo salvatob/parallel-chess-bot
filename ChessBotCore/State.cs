254c4282ead45506b69d86e7f9a42558ad822702
salvat <tobias@avlas.cz>
2026-06-29 18:34:09 +0200

    Redo State default constructor. It now creates the initial position.

--- ChessBotCore/State.cs
+++ ChessBotCore/State.cs
@@ -36,25 +36,28 @@ public class State {
     public const char BlackKnightSymbol = 'n';
     public const char BlackRookSymbol = 'r';
 
-    [Obsolete($"{nameof(State)}.{nameof(Empty)} should be used instead of the constructor", false)]
+    // [Obsolete($"{nameof(State)}.{nameof(Empty)} should be used instead of the constructor", false)]
     public State() {
-        WhitePawns = 0;
-        WhiteRooks = 0;
-        WhiteKnights = 0;
-        WhiteBishops = 0;
-        WhiteQueens = 0;
-        WhiteKing = 0;
-        BlackPawns = 0;
-        BlackRooks = 0;
-        BlackKnights = 0;
-        BlackBishops = 0;
-        BlackQueens = 0;
-        BlackKing = 0;
+        WhitePawns   = 0b_1111_1111_0000_0000;
+        WhiteRooks   = 0b_1000_0001;
+        WhiteKnights = 0b_0100_0010;
+        WhiteBishops = 0b0010_0100;
+        WhiteQueens  = 0b0001_0000;
+        WhiteKing    = 0b_0000_1000;
+            
+        BlackPawns   = 0b_1111_1111UL << (6 * 8);
+        BlackRooks   = 0b_1000_0001UL << (7 * 8);
+        BlackKnights = 0b_0100_0010UL << (7 * 8);
+        BlackBishops = 0b_0010_0100UL << (7 * 8);
+        BlackQueens  = 0b_0001_0000UL << (7 * 8);
+        BlackKing    = 0b_0000_1000UL << (7 * 8);
+            
+        WhiteCastleKingSide  = true;
+        WhiteCastleQueenSide = true;
+        BlackCastleKingSide  = true;
+        BlackCastleQueenSide = true;
         WhiteIsActive = true;
-        WhiteCastleKingSide = false;
-        WhiteCastleQueenSide = false;
-        BlackCastleKingSide = false;
-        BlackCastleQueenSide = false;
+        FullMoves = 1;
     }
 
     public Bitboard WhitePawns { get; set; }
