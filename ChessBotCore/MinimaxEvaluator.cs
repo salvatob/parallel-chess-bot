809b1c33ce114ff9e90143de3b526e5010ea88e7
salvat <tobias@avlas.cz>
2026-07-05 01:04:03 +0200

    Rewrite the minimaxEvaluator

--- ChessBotCore/MinimaxEvaluator.cs
+++ ChessBotCore/MinimaxEvaluator.cs
@@ -11,63 +11,54 @@ public sealed class MinimaxEvaluator {
     }
     
     public Move ChooseBestMove(State state, int maxDepth) {
-        return MinimaxSetup(state, maxDepth);
+        return NegamaxBase(state, maxDepth);
     }
 
-    private Move MinimaxSetup(State state, int maxDepth) {
-        bool isMaxing = state.WhiteIsActive;
+    private Move NegamaxBase(State state, int maxDepth) {
+        var moves = _generator.GenerateMoves(state).ToList();
+        moves.Sort();
         
-        // todo sort the moves somehow 
-        using var moves = _generator.GetLegalMoves(state).ToList().GetEnumerator();
-
-        
-        // querying for a move when stalemated is undefined behaviour
-        if (!moves.MoveNext()) return default;
-
-        
-        int bestScore = isMaxing ? int.MinValue : int.MaxValue;
+        int bestScore =  int.MinValue;
         Move bestMove = default;
+        
+        foreach (var move in moves) {
+            var undo = state.ApplyMove(move);
+            int currentScore = - Negamax(state, maxDepth - 1);
+            state.UndoMove(move, undo);
 
-        do {
-            int currentScore = Minimax(moves.Current.StateAfter, maxDepth - 1);
-            if (isMaxing) {
-                if (currentScore > bestScore) {
-                    bestMove = moves.Current;
-                    bestScore = currentScore;
-                }
+            if (currentScore > bestScore) {
+                bestMove = move;
+                bestScore = currentScore;
             }
-            else {
-                if (currentScore < bestScore) {
-                    bestMove = moves.Current;
-                    bestScore = currentScore;
-                }
-            }
-            
-        } while (moves.MoveNext());
+        };
 
         return bestMove;
     }
 
-    private int Minimax(State state, int depth) {
-        if (depth <= 0 || IsTerminal(state)) return Eval(state);
+    private int Negamax(State state, int depth) {
+        if (depth <= 0 || IsTerminal(state)) {
+            var score = Eval(state);
+            return state.WhiteIsActive ? score : -score;
+        } 
         
-        bool isMaxing = state.WhiteIsActive;
+        int bestScore = int.MinValue;
         
-        int bestScore = isMaxing ? int.MinValue : int.MaxValue;
         
-        // todo sort the moves somehow 
-        using var moves = _generator.GenerateMoves(state).GetEnumerator();
-
-        // no moves means stalemate, which is loss for both players
-        if (!moves.MoveNext()) return 0;
-
-        do {
-            int currentScore = Minimax(moves.Current.StateAfter, depth - 1);
-            bestScore = isMaxing ? int.Max(bestScore, currentScore) : int.Min(bestScore, currentScore);
-
-        } while (moves.MoveNext());
+        var moves = _generator.GenerateMoves(state).ToList();
+        moves.Sort();
         
+        foreach (var move in moves) {
+            var undo = state.ApplyMove(move);
+            int currentScore = - Negamax(state, depth - 1);
+            state.UndoMove(move, undo);
+            
+            bestScore = int.Max(bestScore, currentScore);
+        
+        };
 
         return bestScore;
     }
+
+  
+    
 }
