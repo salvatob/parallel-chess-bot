39a7896fed3d513f7d9527e4ab9a6d50241fd791
salvat <tobias@avlas.cz>
2026-07-04 23:09:33 +0200

    Rewrite Move to be a small struct that is applied to a State

--- ChessBotCore/Move.cs
+++ ChessBotCore/Move.cs
@@ -1,26 +1,41 @@
+using System;
+
 namespace ChessBotCore;
 
-public struct Move {
-    public  State StateAfter { get; set; }
-    public required bool IsCapture { get; init; }
-    public bool IsPromotion { get; init; }
-    public bool IsCheck { get; init; }
-    public bool IsCastle { get; init; }
-    
-    // public string strRepresentation =
-    public Coordinates coordsBefore { get; set; }
-    public Coordinates coordsAfter { get; set; }
-    
-    
-    public Move(State stateAfter) {
-        StateAfter = stateAfter;
+[Flags]
+public enum MoveFlags : uint {
+    None = 0,
+    Capture = 1 << 0,
+    DoublePawnPush = 1 << 1,
+    KingCastle = 1 << 2,
+    QueenCastle = 1 << 3,
+    EnPassant = 1 << 4,
+    Promotion = 1 << 5,
+    PromoteToQueen = 1 << 6,
+    PromoteToRook = 1 << 7,
+    PromoteToBishop = 1 << 8,
+    PromoteToKnight = 1 << 9,
+    IsCheck = 1 << 10
+}
+
+public readonly struct Move {
+    private readonly uint _data;
+
+    public Move(int from, int to, MoveFlags flags = MoveFlags.None) {
+        _data = (uint)(from | (to << 6) | ((uint)flags << 12));
     }
+
+    public int From => (int)(_data & 0x3F);
+    public int To => (int)((_data >> 6) & 0x3F);
+    public MoveFlags Flags => (MoveFlags)(_data >> 12);
+
+    public bool IsCapture => Flags.HasFlag(MoveFlags.Capture);
+    public bool IsPromotion => Flags.HasFlag(MoveFlags.Promotion);
+    public bool IsCastle => Flags.HasFlag(MoveFlags.KingCastle) || Flags.HasFlag(MoveFlags.QueenCastle);
+    public bool IsEnPassant => Flags.HasFlag(MoveFlags.EnPassant);
+    public bool IsCheck => Flags.HasFlag(MoveFlags.IsCheck);
 
     public static string? TryGetNotation(State before, State after) {
         return FenCreator.TryGetMoveNotation(before, after);
-    }
-    
-    public readonly string? TryGetNotation(State before) {
-        return FenCreator.TryGetMoveNotation(before, StateAfter);
     }
 }
