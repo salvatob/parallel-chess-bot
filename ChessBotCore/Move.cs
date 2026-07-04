33a1c7d91979f3116ec6ea1e2d0995bd7213ebd2
salvat <tobias@avlas.cz>
2026-07-05 01:04:31 +0200

    Implement natural ordering for Move struct

--- ChessBotCore/Move.cs
+++ ChessBotCore/Move.cs
@@ -2,32 +2,35 @@ using System;
 
 namespace ChessBotCore;
 
+// Do not move the order of these flags, they define natural ordering so the sorting of Moves is very trivial 
 [Flags]
-public enum MoveFlags : uint {
-    None = 0,
-    Capture = 1 << 0,
-    DoublePawnPush = 1 << 1,
-    KingCastle = 1 << 2,
-    QueenCastle = 1 << 3,
-    EnPassant = 1 << 4,
-    Promotion = 1 << 5,
-    PromoteToQueen = 1 << 6,
+public enum MoveFlags : ushort {
+    Promotion = 1 << 15,
+    Capture = 1 << 14,
+    IsCheck = 1 << 13,
+    DoublePawnPush = 1 << 12,
+    KingCastle = 1 << 11,
+    QueenCastle = 1 << 10,
+    EnPassant = 1 << 9,
+    PromoteToQueen = 1 << 8,
     PromoteToRook = 1 << 7,
-    PromoteToBishop = 1 << 8,
-    PromoteToKnight = 1 << 9,
-    IsCheck = 1 << 10
+    PromoteToBishop = 1 << 6,
+    PromoteToKnight = 1 << 5,
+    None = 0
 }
 
-public readonly struct Move {
+public readonly struct Move : IComparable<Move> {
     private readonly uint _data;
 
     public Move(int from, int to, MoveFlags flags = MoveFlags.None) {
-        _data = (uint)(from | (to << 6) | ((uint)flags << 12));
+        _data = (uint)(from | (to << 6) | ((uint)flags << 16));
     }
 
     public int From => (int)(_data & 0x3F);
     public int To => (int)((_data >> 6) & 0x3F);
-    public MoveFlags Flags => (MoveFlags)(_data >> 12);
+    public MoveFlags Flags => (MoveFlags)(_data >> 16);
+
+    public int CompareTo(Move other) => other._data.CompareTo(_data); // Descending for high-priority moves first
 
     public bool IsCapture => Flags.HasFlag(MoveFlags.Capture);
     public bool IsPromotion => Flags.HasFlag(MoveFlags.Promotion);
