301d7b35f41a71c686041a7366054a9b160cd0a5
salvat <tobias@avlas.cz>
2026-07-07 22:21:07 +0200

    update main method

--- ConsoleInterface/Program.cs
+++ ConsoleInterface/Program.cs
@@ -8,13 +8,15 @@ internal class Program {
     public static void Main(string[] args) {
         var chessSingle = new DefaultChessWrapper();
         var chessMulti = new ParallelChessWrapper();
-
+        // State state = State.FromFen("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"); 
+        // State state = State.FromFen("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1 "); 
+        State state = State.FromFen("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"); 
         int depth = 6; // I want 119,060,324 nodes without castles
-        Console.WriteLine("single threaded");
-        TryPerft(depth, chessSingle);
+        // Console.WriteLine("single threaded");
+        // TryPerft(depth, chessSingle);
         
         Console.WriteLine("multi threaded");
-        TryPerft(depth, chessMulti);
+        PerftStats(depth, chessMulti, state);
 
         
         // DividePerft(State.Initial, 3, chessSingle);
@@ -49,27 +51,25 @@ internal class Program {
         Console.WriteLine($"Nodes searched: {moveCounts.Values.Sum()}");
     }
     
-    static void TryPerft(int depth, IChessWrapper chess) {
-        // void TryPerft(int depth, Func<State, int, long> perft) {
- 
+    static void PerftStats(int depth, IChessWrapper chess, State? state=null) {
+        state ??= State.Initial;
         GC.Collect();
         GC.WaitForPendingFinalizers();
         GC.Collect();
 
         var sw = Stopwatch.StartNew();
 
-        // var memoryBefore = GC.GetAllocatedBytesForCurrentThread();
-        // var leavesExplored = chess.EvalPerft(State.Initial, depth);
-        var leavesExplored = chess.Perft(State.Initial, depth);
+        var memoryBefore = GC.GetAllocatedBytesForCurrentThread();
+        var leavesExplored = chess.Perft(state, depth);
 
-        // var memoryAfter = GC.GetAllocatedBytesForCurrentThread();
+        var memoryAfter = GC.GetAllocatedBytesForCurrentThread();
         sw.Stop();
-        // var mem = memoryAfter - memoryBefore;
+        var mem = memoryAfter - memoryBefore;
 
 
         Console.WriteLine($"Pertf has explored {leavesExplored:N0} leaves in depth {depth} in {sw.Elapsed.ToString()}.");
         Console.WriteLine($"Comes up to around {(leavesExplored * 1000) / sw.ElapsedMilliseconds:N0} leaves per second");
-        // Console.WriteLine($"{mem/1000_000} MB of memory has been allocated during");
+        Console.WriteLine($"{mem/1000_000} MB of memory has been allocated during");
     }
 }
 
