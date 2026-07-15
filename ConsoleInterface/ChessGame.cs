27e93f7defccc1387e8fe81aceacd26dad574d71
salvat <tobias@avlas.cz>
2026-07-15 15:55:26 +0200

    Implement ChessGame class

--- ConsoleInterface/ChessGame.cs
+++ ConsoleInterface/ChessGame.cs
@@ -10,7 +10,7 @@ public class ChessGame {
     private readonly IPlayer _blackPlayer;
     private readonly IPlayer _whitePlayer;
     private readonly State _state = State.Initial;
-    private Timers _timers = new();
+    private readonly Timers _timers = new();
 
     public ChessGame(IPlayer whitePlayer, IPlayer blackPlayer) {
         _whitePlayer = whitePlayer;
@@ -21,9 +21,48 @@ public class ChessGame {
         return isWhite ? _whitePlayer : _blackPlayer;
     }
 
-    public async Task<List<Move>> Play(int verbosity) {
-        while (!_state.IsTerminal()) { }
+    public record class GameResult(GameResult.GameOutcome Outcome, List<Move> Moves) {
+        public enum GameOutcome {
+            WhiteWin,
+            BlackWin,
+            Draw
+        }
+           
+        
+    }
+    
+    public async Task<GameResult> Play(int verbosity) {
+        List<Move> moveList = new();
+        while (!_state.IsTerminal()) {
+            var player = ActivePlayer(_state.WhiteIsActive);
+            Console.WriteLine($"Player {player.GetType().Name} turn");
+            Console.WriteLine(_state.PrettyPrint());
+            
+            var moveHandle = player.GetBestMove(_state, _timers);
+           
+            // var cts = new CancellationTokenSource();
+            // Task.Run(async () => {
+            //     await Task.Delay(TimeSpan.FromSeconds(10));
+            //     Console.WriteLine("Time limit reached");
+            //     moveHandle.Cancel();
+            // }, cts.Token);
+            //
+            // moveHandle.Register(cts.Cancel);
+            
+            
+            var searchResult = await moveHandle.Result;
 
-        throw new NotImplementedException();
+
+            var move = searchResult.BestMove;
+            
+            Console.WriteLine($"Player {player.GetType().Name} made move {move.PrintUCI()}");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            moveList.Add(move);
+            _state.ApplyMove(move);
+        }
+
+        return new (GameResult.GameOutcome.Draw ,moveList);
     }
 }
